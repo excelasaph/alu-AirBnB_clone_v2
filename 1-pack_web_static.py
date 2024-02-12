972bc38384bfec6f856721fdd2927df9e50b5cb7
Emmanuel Obolo <e.obolo@alustudent.com>
2024-02-12 16:04:31 +0000

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -25,12 +25,12 @@ def do_pack():
     date_obj = datetime.now()
     date_str = date_obj.isoformat()
     char_rem = "-:.T"
-    count = 0;
+    count = 0
     try:
-    	while char_rem[count] in date_str:
+        while char_rem[count] in date_str:
             date_str = date_str.split(char_rem[count])
             date_str = "".join(date_str)
             count += 1
     except IndexError:
         pass
-    fab.local("tar -czvf web_static_{0}.tar.gz web_static".format(date_str))
+    fab.local("tar -czvf web_static_{0}.tgz web_static".format(date_str))
