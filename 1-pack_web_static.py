63a79d457a5a15911aeadb9784148e4bffb53fee
Emmanuel Obolo <e.obolo@alustudent.com>
2024-02-12 15:59:38 +0000

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,27 @@
+#!/usr/bin/python3
+"""
+Write a Fabric script that generates
+a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo,
+using the function do_pack.
+"""
 import fabric.api as fab
 from datetime import datetime
 
 
 def do_pack():
+    """
+    Prototype: def do_pack():
+    All files in the folder web_static must
+    be added to the final archive
+    All archives must be stored in the folder versions
+    (your function should create this folder if it doesn't exist)
+    The name of the archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
+    The function do_pack must return the archive path
+    if the archive has been correctly generated.
+    Otherwise, it should return None
+    """
     date_obj = datetime.now()
     date_str = date_obj.isoformat()
     char_rem = "-:.T"
@@ -14,6 +33,4 @@ def do_pack():
             count += 1
     except IndexError:
         pass
-    else:
-    	print(date_str)
     fab.local("tar -czvf web_static_{0}.tar.gz web_static".format(date_str))
