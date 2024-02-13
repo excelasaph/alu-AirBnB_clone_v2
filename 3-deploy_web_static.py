9454058e88e05bbc274c0dacdfd2b8baead92720
Emmanuel Obolo <e.obolo@alustudent.com>
2024-02-13 19:02:00 +0000

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Write a Fabric script
+(based on the file 2-do_deploy_web_static.py)
+that creates and distributes
+an archive to your web servers,
+using the function deploy:
+"""
 from fabric.api import env, execute
 
 
@@ -7,6 +14,19 @@ do_deploy = __import__("2-do_deploy_web_static").do_deploy
 
 
 def deploy():
+    """
+    Prototype: def deploy():
+    The script should take the following steps:
+    Call the do_pack() function and store the path
+    of the created archive Return False if no archive
+    has been created Call the do_deploy(archive_path)
+    function, using the new path of the new archive
+    Return the return value of do_deploy
+    All remote commands must be executed on both of web
+    your servers (using env.hosts = ['<IP web-01>', 'IP web-02']
+    variable in your script) You must use this script to deploy
+    it on your servers: xx-web-01 and xx-web-02
+    """
     archive_path = do_pack()
     results = execute(do_deploy, archive_path=archive_path)
     return results
